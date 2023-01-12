8d6f1584af578339be56ab4d2a57f7135d5c7b93
edrine13 <edrine.rubio@gmail.com>
2023-01-12 17:40:02 +0800

    CRU D:assignment

--- server.js
+++ server.js
@@ -22,37 +22,19 @@ mongoose
 
 // console.log(process.env);
 
-const tourSchema = new mongoose.Schema({
-  name: {
-    type: String,
-    required: [true, 'A tour must have a name'],
-    unique: true,
-  },
-  rating: {
-    type: Number,
-    default: 4.5,
-  },
-  price: {
-    type: Number,
-    required: [true, 'A tour must have a price'],
-  },
-});
+// const testTour = new Tour({
+//   name: 'The Park Camper',
+//   price: 997,
+// });
 
-const Tour = mongoose.model('Tour', tourSchema);
-
-const testTour = new Tour({
-  name: 'The Park Camper',
-  price: 997,
-});
-
-testTour
-  .save()
-  .then((doc) => {
-    console.log(doc);
-  })
-  .catch((err) => {
-    console.log('ERROR 💥', err);
-  });
+// testTour
+//   .save()
+//   .then((doc) => {
+//     console.log(doc);
+//   })
+//   .catch((err) => {
+//     console.log('ERROR 💥', err);
+//   });
 
 const port = process.env.PORT || 8000;
 app.listen(port, () => {
